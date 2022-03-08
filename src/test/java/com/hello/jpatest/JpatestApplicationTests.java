94a8b6293734fcd3a14be3db3a2a8b95f39a6980
ksoon1985 <ksoon1985@naver.com>
2022-03-08 17:42:59 +0900

    springboot jpa 초기 셋팅

---
+++ src/test/java/com/hello/jpatest/JpatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.jpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
